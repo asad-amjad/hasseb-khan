1d6e741696c989969714a480164530452eedad03
Asad <57917600+asad-amjad@users.noreply.github.com>
2025-04-04 18:20:24 +0500

    fix bug

--- src/screens/body-aesthetics/Bbl.jsx
+++ src/screens/body-aesthetics/Bbl.jsx
@@ -9,8 +9,8 @@ import ImageComparisonSlider from "../../components/image-comparison-slider/Imag
 import { storiesData } from "../../../constants";
 
 // Assets
-// import bannerImage from "../../assets/body-aesthetics/bbl.jpg";
-// import introImage from "../../assets/body-aesthetics/bbl-1.jpg";
+import bannerImage from "../../assets/body-aesthetics/bbl.jpg";
+import introImage from "../../assets/body-aesthetics/bbl-1.jpg";
 
 const Bbl = () => {
   return (
@@ -20,22 +20,22 @@ const Bbl = () => {
         title="Bbl"
         description="Our orthodontic treatments offer advanced solutions for teeth alignment and bite correction. From traditional braces to modern Invisalign, we provide tailored options to enhance your smile’s aesthetics and functionality."
       />
-      {/* <div className="md:pt-[100px]">
+     <div className="md:pt-[100px]">
         <img
           className="object-cover w-full h-398px md:h-[860px] md:max-h-[860px]"
           src={bannerImage}
           alt="image"
         />
-      </div> */}
+      </div> 
 
-      {/* <IntroWithImage
+      <IntroWithImage
         badge="Introduction"
         title="About Orthodontics"
         descriptions={[
           "Orthodontics focuses on correcting misaligned teeth and jaw structures to improve oral health, aesthetics, and functionality. Through innovative treatments like Damon Smile Braces and Invisalign, we ensure effective and comfortable solutions for achieving a beautifully aligned smile.",
         ]}
         imageSrc={introImage}
-      /> */}
+      /> 
 
       <ServiceDetails
         title="Comprehensive Services"
